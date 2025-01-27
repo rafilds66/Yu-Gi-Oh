b8520bd20c4995fb5fc23cff360168c0de9da2fe
R4f1lds <rafilds66@gmail.com>
2025-01-26 21:43:58 -0300

    commitando código js e ajustes no estilo e responsivo

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+const setaAvancar = document.getElementById('botao-avancar');
+const setaVoltar = document.getElementById('botao-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+  cartao.addEventListener('click', function(){
+    const cartaVirada = cartao.querySelector('.carta-virada');
+    cartao.classList.toggle('virar');
+    cartaVirada.classList.toggle('mostrar-fundo-carta');
+
+    const descricao = cartao.querySelector('.descricao');
+    descricao.classList.toggle('esconder');
+  });
+});
+
+setaAvancar.addEventListener('click', function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+  esconderCartaoSelecionado();
+  cartaoAtual++;
+  mostraCartao();
+});
+
+  setaVoltar.addEventListener('click', function () {
+    if (cartaoAtual === 0) return;
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    mostraCartao();
+  });
+
+
+function mostraCartao() {
+  cartoes[cartaoAtual].classList.add('selecionado');
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector('.selecionado');
+  cartaoSelecionado.classList.remove('selecionado');
+}
+
